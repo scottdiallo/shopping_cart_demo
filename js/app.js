65470ec055d951ff941f5f192ceed9d4156fd780
scottdiallo <diallot@me.com>
2016-04-12 09:43:03 -0700

    basic functionality created

--- js/app.js
+++ js/app.js
@@ -1,4 +1,20 @@
+
+$(document).ready(function() {
+	$('.form-container').on('submit',function(event){
+		event.preventDefault();
+		var quantity = $('#quantity').val();
+		var description = $('#description').val();
+		var price = '$' + $('#price').val();
+		$('.list').append('<li class="item">' + quantity + " " + description + " " + price + '</li>')
+	});
+
+})
+
+
+
+
 // form on submit
+
    // get form values
    // create li element
    // append element to list
