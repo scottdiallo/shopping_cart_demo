b3f2711e30201b51c53a01e138dca5f913d72968
OoogleBoogle <simattfield@gmail.com>
2016-04-12 07:59:51 -0700

    Added psuedo code for html and js

---
+++ js/app.js
@@ -0,0 +1,10 @@
+// form on submit
+   // get form values
+   // create li element
+   // append element to list
+
+// list item on click
+  // if item has a class of complete
+    // delete item
+  // else 
+    // make complete (class)
